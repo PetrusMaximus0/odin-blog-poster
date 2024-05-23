e333c10bcb882f881a810383028350843cabeb32
PetrusMaximus0 <45740112+PetrusMaximus0@users.noreply.github.com>
2024-05-23 12:33:51 +0200

    feat: LoginForm stores response token on a successfull sign in, redirects to main page.
    - The token returned in a successfull form submission response is stored in local storage. After storage the component redirects to the main page.
    
    - A 'form submitting' message is rendered while fetching is pending.
    
    - form submission callback is refactored to use try catch. Was done for practice.

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,74 +1,88 @@
 import { FormEvent, useState } from "react";
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import CustomInput from "./CustomInput";
 
 export default function LoginForm() {
     interface LoginFormState {
         username: string,
         password: string,
     }
+    const [formSubmitting, setFormSubmitting] = useState<boolean>(false);
+    const [formError, setFormError] = useState<string | null>(null);
+    const [formData, setFormData] = useState<LoginFormState>({} as LoginFormState);
 
-    const [formResponse, setFormResponse] = useState<null | object>(null);
-    const [formError, setFormError] = useState <string | null>(null);
-    const [formData, setFormData] = useState<LoginFormState>({
-        username: "",
-        password: "",
-    })
+    const navigate = useNavigate();
 
     function handleInputChange(e: FormEvent<HTMLInputElement>) {
         const { name, value } = e.currentTarget;
         setFormData({ ...formData, [name]: value });
     }
 
-    function handleFormSubmit(e: FormEvent) {
+    const storeToken = (token: string) => {
+        localStorage.setItem("login-token", token);
+    }
+
+    const handleFormSubmit = async (e: FormEvent) => {
         e.preventDefault();
-        fetch("http://localhost:3000/users/login", {
-            mode: "cors",
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(formData),
-        })
-            .then((res) => {
-                if (!res.ok) {
-                    throw new Error(`Error with status: ${res.status}`);
-                }
-                return res.json();                
+        setFormSubmitting(true);     
+            
+        try {
+            const result = await fetch("http://localhost:3000/users/login", {
+                mode: "cors",
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
             })
-            .then((res) => {
-                setFormResponse(res);                
-                setFormData({username: "", password: ""})
-            })
-            .catch((error) => {
-                console.log(error.message);
-                setFormError(error.message);
-            })
+            
+            const res = await result.json();
+            
+            //
+            if (!result.ok) {               
+                setFormSubmitting(false);
+                throw new Error(res.error);
+            }
+
+            // 
+            if (res.token !== undefined) {
+                storeToken(res.token);
+                setFormData({ username: "", password: "" })
+                navigate("/");
+            }
+
+            //
+            setFormError(null);
+            setFormSubmitting(false);      
+            
+
+        } catch (error) {
+            setFormError((error as Error).message);
+            setFormSubmitting(false);
+        }
     }
 
     return (
-        <div>
-            <form onSubmit={handleFormSubmit} className="border container max-w-[500px] w-fit mx-auto p-4 my-4" aria-label="login-form" name="login-form">
-                <fieldset className="flex flex-col gap-2 py-4 items-center">
-                    <legend className="mx-auto w-auto pt-2 px-2 pb-4">
-                        Log In
-                    </legend>
-                    <label htmlFor="username">Username
-                        <input required onChange={(e)=>handleInputChange(e)} value={formData.username} className="ml-2 bg-slate-700 white px-2" type="text" name="username" id="username" />
-                    </label>
-                    <label htmlFor="password">Password
-                        <input required onChange={(e)=>handleInputChange(e)} value={formData.password} className="ml-2 bg-slate-700 white px-2" type="password" name="password" id="password" />
-                    </label>
-                    <div className="w-full mt-4 flex justify-between gap-2">
-                        <Link className="border px-3 py-1" to="/"> Create Account </Link>
-                        <button className="border px-3 py-1" type="submit">Log in</button>
-                    </div>
-                </fieldset>
-                {formError !== null && 
-                    <p className="text-red-300"> {formError} </p>
-                }
-            </form>
-        
-        </div>
-    
+        <form onSubmit={handleFormSubmit} className="border container w-fit mx-auto p-4 my-4" aria-label="login-form" name="login-form">
+            <fieldset className="flex flex-col gap-2 py-4 mx-auto items-center">
+                <legend className="mx-auto w-auto pt-2 px-2 pb-4">
+                    Log In
+                </legend>
+                <label className="w-full flex justify-between gap-10" htmlFor="username">Username
+                    <CustomInput required={true} name="username" value={formData.username} inputType="text" handleInputChange={handleInputChange} /> 
+                </label>
+                <label className="w-full flex justify-between gap-10" htmlFor="password">Password
+                    <CustomInput required={true} name="password" value={formData.password} inputType="password" handleInputChange={handleInputChange} />
+                </label>
+                <div className="w-full mt-4 flex justify-between gap-2">
+                    <Link className="border px-3 py-1" to="/newuser"> Create Account </Link>
+                    <button className="border px-3 py-1" type="submit">Log in</button>
+                </div>
+            </fieldset>
+            {formError !== null && 
+                <p className="text-red-300"> {formError} </p> ||
+                formSubmitting && <p> "Submitting, please wait..."</p>
+            }
+        </form>    
     )
 }   
