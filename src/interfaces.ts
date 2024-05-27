5288fb487b45532e8e45c3270e43cf534b0cc880
PetrusMaximus0 <45740112+PetrusMaximus0@users.noreply.github.com>
2024-05-27 12:55:19 +0200

    feat: Add interfaces for the Catalog loader data, Posts, Comments, Categories. Create a type alias for form input change events for more concise code.

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -4,6 +4,28 @@ export interface IPosts {
     title: string,
 }
 
+export interface IComment {
+    name: string,
+}
+
+export interface ICategory{
+    name: string,
+}
+
+export interface IPost {
+    _id: string,
+    author: string,
+    title: string,
+    text: string,
+    description: string,
+    timeToRead: string,
+    hidden: string,
+    headerImage: string,
+    date?: string,
+    comments?: IComment[],
+    categories?: string[],    
+}
+
 export interface IUser {
     "posts": IPosts[],
     "_id": string,
@@ -18,10 +40,18 @@ export interface LoginFormState {
     password: string,
 }
 
+export type IInputChange = FormEvent<HTMLInputElement | HTMLTextAreaElement>;
+
 export interface IInputProps{
-    handleInputChange: (e: FormEvent<HTMLInputElement>) => void,
+    handleInputChange: (e: IInputChange) => void,
     value: string,
     name: string,
     required: boolean,
     inputType: string,
+}
+
+export interface ICatalogLoaderData{
+    posts: IPosts[],
+    pageNumber: number,
+    lastPage: number,
 }
