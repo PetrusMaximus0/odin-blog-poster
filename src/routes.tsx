d7dfc6445d32ecdcd874d99533b68f5c3397f1dd
PetrusMaximus0 <45740112+PetrusMaximus0@users.noreply.github.com>
2024-05-27 13:04:36 +0200

    feat: Add routes for the landing page, post viewing,  new post creation, and a catchall redirect page. Add loaders for the aforementioned routes.
    Routes:
    
    feat: Add routes for viewing posts by search term or categories.
    feat: Add route to view post by ID.
    feat: Add route to create a new post.
    feat: Catch all route for routes that are children of /post and don't match any implemented route.
    
    Loaders:
    
    feat: Add rootLoader for the Parent /posts route that provides data to the Content element.
    feat: Add a contentLoader for the catalog routes which provides data for the catalog about which posts to render.

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,122 @@
-import App from "./App";
 import ProtectedRoute from "./components/ProtectedRoute"
 import Login from "./components/LoginForm";
 import CreateUser from "./components/CreateUser";
+import Catalog from "./components/Catalog";
+import Content from "./components/Content";
+import { Navigate, redirect } from "react-router-dom";
+import CreatePost from "./components/CreatePost";
+import QueryCatalog from "./components/QueryCatalog";
+
+const rootLoader = async () => {
+    try {
+
+        const token = localStorage.getItem("login-token");
+        if (!token) {
+            redirect("/");
+            return;
+        }
+        //
+        const result = await fetch("http://localhost:3000/posts/admin/shortlist", {
+            method: "GET",
+            mode: "cors",
+            headers: {
+                "content-type": "application/json",
+                Authorization: `Bearer ${token}`,
+            }
+        })
+
+        //
+        if (result.status >= 400) {
+            if (result.status === 401 || result.status === 404) {
+                redirect("/login");
+                return;
+            } else {
+                throw new Error(`Server error with status, ${result.status}`);
+            }
+        }
+
+        const { posts, categories } = await result.json();
+        const username = localStorage.getItem("login-username");
+
+        return { username: username, posts: posts, categories: categories };
+        
+        
+    } catch (error) {
+        console.error(error);
+    }
+
+
+}
+
+interface IContentLoader {
+    itemNumber?: number,
+    pageNumber?: number,
+    queryType?: string,
+    query?: string,
+    name?: string,
+}
+
+const contentLoader = async ({params} :{ params: IContentLoader}) => {
+    //
+	if (!params.itemNumber) {
+		params.itemNumber = 3;
+	}
+
+	//
+	if (!params.pageNumber || isNaN(params.pageNumber)) {
+		params.pageNumber = 1;
+	}
+
+	const baseUrl = `http://localhost:3000/posts/admin/all?page=${params.pageNumber}&items=${params.itemNumber}`;
+	let url = baseUrl;
+
+	//
+	if (params.queryType) {
+        url += `&queryType=${params.queryType}&query=${params.query}`;
+	}
+
+    //    
+    const token = localStorage.getItem("login-token");
+    if (!token) {
+        redirect("/login");
+    }
+
+	const posts = await fetch(url, {
+		mode: 'cors',
+		method: 'GET',
+		headers: {
+            'content-type': 'application/json',
+            Authorization: `Bearer ${token}`,
+        },
+	})
+		.then((res) => {
+            if (res.status >= 400) {
+                if (res.status === 401 || res.status === 404) {
+                    redirect("/login");
+                    return;
+                } else {
+                    console.log("Throwing error!");
+                    throw new Error(`Server error with status, ${res.status}`);
+                }
+			}
+			return res.json();
+		})
+		.then((res) => {
+			return res;
+		})
+		.catch((error) => console.error(error));
+
+    const username = localStorage.getItem("login-username");
+
+    return {
+        username: username,
+        categories: posts.categories,
+		posts: posts.allPosts,
+		page: params.pageNumber,
+		lastPage: posts.lastPage,
+        queryName: params.name ? params.name : '', 
+	};
+}
 
 const routes = [
     {
@@ -12,12 +127,45 @@ const routes = [
         children: [            
             {
                 path: "/",
-                element: <App />                        
+                element: <Navigate to="/posts/page/1" />,
             },
-            {
-                path: "/1",
-                element: <div>Sub Route 2</div>                        
+            {    
+                loader: rootLoader,
+                path: "/posts",
+                element: <Content />,
+                children: [
+                    {      
+                        loader: contentLoader,
+                        path: "/posts/page/:pageNumber",
+                        element: <Catalog/>
+                    },
+                    {                  
+                        path: "/posts/:id",
+                        element: <p>Loading blogpost</p>
+                    },
+                    {
+                        loader: rootLoader,
+                        path: "/posts/new",
+                        element: <CreatePost/>
+                    },
+                    {
+                        loader: contentLoader,
+                        path: '/posts/:queryType/:query/:name/page/:pageNumber/',            
+                        element: <QueryCatalog />,
+                    },
+                    {
+                        loader: contentLoader,
+                        path: '/posts/:queryType/:query/page/:pageNumber/',
+                        element: <QueryCatalog />,
+                    },
+                    {
+                        path: "/posts/*",
+                        element: <Navigate to="/posts/page/1" />,
+                    }
+                ]
+                
             },
+
         ]        
     },
     {
